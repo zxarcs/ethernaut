f0453f6405d812a8c823c9ad57ebaf4f9aa9b7ea
skcrypto10 <skcrypto10@gmail.com>
2023-01-24 15:59:32 -0500

    solved challenge 18

--- contracts/contracts/attacks/N18MagicNumberAttacker.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract N18MagicNumberAttacker {
-    function whatIsTheMeaningOfLife() public pure returns (uint8) {
-        return 42;
-    }
-}
--- contracts/test/solutions/n18-magicnumber.test.js
+++ contracts/test/solutions/n18-magicnumber.test.js
@@ -1,7 +1,6 @@
 
 const MagicNumFactory = artifacts.require('./levels/MagicNumFactory.sol')
 const MagicNum = artifacts.require('./levels/MagicNum.sol')
-const MagicNumAttacker = artifacts.require('./attcks/N18MagicNumberAttacker.sol')
 const utils = require('../utils/TestUtils')
 const { expect } = require("chai")
 
@@ -11,12 +10,10 @@ describe.only("Challenge 18 - Magic Number", function () {
     let level
     let instance
     let attacker
-    let attackerAltAddress
 
     async function createLevelInstance() {
-        const [_attacker, _alt] = await web3.eth.getAccounts()
+        const [_attacker] = await web3.eth.getAccounts()
         attacker = _attacker
-        attackerAltAddress = _alt
 
         ethernaut = await utils.getEthernautWithStatsProxy()
         level = await MagicNumFactory.new()
@@ -38,14 +35,81 @@ describe.only("Challenge 18 - Magic Number", function () {
         /*
             Contract logic will consist of saving a number (i.e. 42, or 0x2a in hex) and
             then returning it. We will accomplish this with opcodes MLOAD and RETURN
+
+            RUNTIME OPCODES
+
+                Load the number to memory
+                --------------------------
+                0x60 (PUSH1)  0x2a (number 42).                   Final code = 0x602a
+                0x60 (PUSH1)  0x80 (mem offset).                  Final code = 0x6080
+                0x52 (MSTORE) Takes two previous codes as
+                    input and stores the number 42 starting
+                    at memory offset 0x80.                        Final code = 0x52
+                .................................
+                Section final code = 0x602a608052
+                .................................
+                
+                
+                Return the stored number
+                --------------------------
+                0x60 (PUSH1)  0x20 (32 -size of uint256 ).        Final code = 0x6020
+                0x60 (PUSH1)  0x80 (mem offset).                  Final code = 0x6080
+                0xf3 (RETURN) Takes two previous codes as
+                    input, stops execution and returns
+                    size of 32 bytes. As indicated by the
+                    first push above                              Final code = 0xf3
+                .................................
+                Section final code = 0x60206080f3
+                .................................
+
+
+                Final code for our contract:
+                0x602a60805260206080f3
+
+                10 opcodes! That is the maximum in order to pass this challenge.
+
+                That's it! we have "written" our contract. Hopefully this makes you
+                appeciate writing these in Solidity instead manually like this :D
+
+            DEPLOYING THE CONTRACT
+
+                Now lets work on more opcodes that will take our contract above and
+                load it into EVM. We will do very similar like we did above by
+                using MSTORE and RETURN
+
+                0x69 (PUSH10) 0x602a60805260206080f3 (Our "contract").
+                    Our contract is 10 bytes long, so we are using
+                    PUSH10 opcode.                                              Final code = 0x69602a60805260206080f3
+                0x60 (PUSH1) 0x20 (mem offset)                                  Final code = 0x6020
+                0x52 (MSTORE) Takes two previous codes as
+                    input and stores the contract code
+                    starting at memory offset 0x20.                             Final code = 0x52
+                0x60 (PUSH1) 0x0a (10 -the size of out contract)                Final code = 0x600a
+                0x60 (PUSH1) 0x36 (mem offset, decimal number 30)
+                    This is calculated by starting offset (0x20) +
+                    (32 bytes - code size, which is 10 bytes) =
+                    0x20 + 22 bytes = 0x20 + 0x16 = 0x36                        Final code = 0x6036
+                0xf3 (RETURN) Takes two previous codes as
+                    input, stops execution and returns
+                    size of 10 bytes. As indicated by the
+                    push above                                                  Final code = 0xf3
+                .................................
+                Final code = 0x690x602a60805260206080f3602052600a6036f3
+                .................................
         */
 
         it("construct contract manually", async function () {
-            const maliciousContract = await MagicNumAttacker.new()
-            await instance.setSolver(maliciousContract.address)
+            const finalByteCode = "0x69602a60805260206080f3602052600a6036f3"
 
-            const maliciousContractBytecode = await web3.eth.getCode(maliciousContract.address)
-            console.log("bytecodes::", maliciousContractBytecode);
+            const data = {
+                from: attacker,
+                data: finalByteCode
+            }
+
+            const solverAddress = (await web3.eth.sendTransaction(data)).contractAddress        
+            await instance.setSolver(solverAddress)
+
+            expect(solverAddress).to.contain("0x")
         })
     })
 
